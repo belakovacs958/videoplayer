db9f0425229a93f4ceeee2baa21c2226f3f7cca4
kovac <kovacsbela958@gmail.com>
2020-01-14 10:11:37 +0100

    class controller: added buttons GUI: added list view

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.application.Application;
+import javafx.scene.control.Button;
+
+
+
 public class Controller {
+    @FXML
+    private Button play_btn, pause_btn, stop_btn;
+
 }
